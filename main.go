957ca0ad8fe229c2ab0d388483ec0568e5280945
wilson <wwilson008@gmail.com>
2023-01-17 19:26:34 +0800

    complete

--- main.go
+++ main.go
@@ -11,34 +11,36 @@ import (
 )
 
 func main() {
-	f, err := os.Open("保險.csv")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
+	for {
+		f, err := os.Open("保險.csv")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer f.Close()
 
-	r := csv.NewReader(f)
-	records, err := r.ReadAll()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(records)
+		r := csv.NewReader(f)
+		records, err := r.ReadAll()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(records)
 
-	receipts := make([]receipt.Receipt, 0, 100)
-	for _, item := range records[1:] {
-		money, _ := strconv.Atoi(item[1])
-		doc := receipt.NewReceipt(util.StrToTime(item[0]), money, item[2])
-		receipts = append(receipts, doc)
-		fmt.Print(doc)
+		receipts := make([]receipt.Receipt, 0, 100)
+		for _, item := range records[1:] {
+			money, _ := strconv.Atoi(item[1])
+			doc := receipt.NewReceipt(util.StrToTime(item[0]), money, item[2])
+			receipts = append(receipts, doc)
+			fmt.Print(doc)
+		}
+		fmt.Print("最大金額: $ ")
+		var dollar int
+		fmt.Scanln(&dollar)
+		fmt.Print("幾個部位: ")
+		var partial int
+		fmt.Scanln(&partial)
+		c := receiptClass.NewReceiptClass(receipts, dollar, partial)
+		NoneDuplicateMethod(c)
 	}
-	fmt.Print("最大金額: $ ")
-	var dollar int
-	fmt.Scanln(&dollar)
-	fmt.Print("幾個部位: ")
-	var partial int
-	fmt.Scanln(&partial)
-	c := receiptClass.NewReceiptClass(receipts, dollar, partial)
-	NoneDuplicateMethod(c)
 }
 
 func NoneDuplicateMethod(receiptClass receiptClass.ReceiptClass) {
